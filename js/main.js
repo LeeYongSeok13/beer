a4baf00f55643641bc8ffb9c6c78197676518ece
LeeYongSeok13 <minamiharuka333@gmail.com>
2022-08-02 17:49:25 +0900

    20220802

--- js/main.js
+++ js/main.js
@@ -3,5 +3,9 @@ $(function () {
         e.preventDefault();
         var hash = $(this.hash).offset().top;
         $('html,body').animate({ scrollTop: hash }, 3000)
-    })
+    });
+
+    $('.beer_slider').slick({
+        arrows: false,
+    });
 })
