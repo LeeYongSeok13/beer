59509702d29e03bb84a59e15dc61210a5e8408b5
LeeYongSeok13 <minamiharuka333@gmail.com>
2022-08-05 17:09:43 +0900

    20220805

--- js/main.js
+++ js/main.js
@@ -5,19 +5,6 @@ $(function () {
         $('html,body').animate({ scrollTop: hash }, 3000)
     });
 
-    $('.toTop').on('click', function () {
-        $('html,body').animate({ scrollTop: 0 }, 200);
-    });
-
-    $(window).on('scroll', function () {
-        var sct = $(window).scrollTop();
-        if (sct > 500) {
-            $('.toTop').fadeIn(300);
-        } else {
-            $('.toTop').fadeOut(1000);
-        }
-    });
-
     $('.product_slider').slick({
         arrows: false,
         autoplay: true,
@@ -31,6 +18,28 @@ $(function () {
     $('.beer_slider').slick({
         arrows: false,
         autoplay: true,
+    });
+
+    $(window).on('scroll', function () {
+        var box = $(window).scrollTop();
+        if (box > 300) {
+            $('.event .box').fadeIn(300);
+        } else {
+            $('.event .box').fadeOut(1000);
+        }
+    })
+
+    $('.toTop').on('click', function () {
+        $('html,body').animate({ scrollTop: 0 }, 200);
+    });
+
+    $(window).on('scroll', function () {
+        var sct = $(window).scrollTop();
+        if (sct > 500) {
+            $('.toTop').fadeIn(300);
+        } else {
+            $('.toTop').fadeOut(1000);
+        }
     });
 
 })
